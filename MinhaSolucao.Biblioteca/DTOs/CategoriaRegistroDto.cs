597e09ea90c60b8ac8d4097f708c607ace48f444
MarianaSlm <marianasoareslima177@gmail.com>
2026-01-30 16:09:22 -0300

    chore(biblioteca): altera a visibilidade da classe 'CategoriaRegistroDto'

--- MinhaSolucao.Biblioteca/DTOs/CategoriaRegistroDto.cs
+++ MinhaSolucao.Biblioteca/DTOs/CategoriaRegistroDto.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MinhaCantina.Biblioteca.DTOs
 {
-	class CategoriaRegistroDto
+	public class CategoriaRegistroDto
 	{
 		public string Nome { get; set; } // Propriedade
 	}
